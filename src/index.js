36c019d9a9a79727a8fdf2b5585c8bf149e9dfed
Julien Ganichot <julien.ganichot@gmail.com>
2016-09-21 15:27:44 +0200

    Loading from .uniwraprc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import Uniwrap from './uniwrap';
+import path from 'path';
 
-export default Uniwrap;
+const path = path.resolve(__dirname, '.uniwraprc');
+console.log(path);
+const wrp = new Uniwrap(path);
+
+export default wrp;
