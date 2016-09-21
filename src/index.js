218b5e583476dc1a628a2426959da69a04709118
Julien Ganichot <julien.ganichot@gmail.com>
2016-09-21 16:34:31 +0200

    Back to square one

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import Uniwrap from './uniwrap';
 
-let instance = null;
-
-const root = `${process.cwd()}.uniwraprc`
-console.log('Root:', root);
-console.log(__dirname);
-const wrp = new Uniwrap(root);
-
-export default wrp;
+export default Uniwrap;
