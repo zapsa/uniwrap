841dcfe9d598b0ad2b8eebb4af8cdd0c99a162ef
Julien Ganichot <julien.ganichot@gmail.com>
2016-09-21 15:53:39 +0200

    RC File fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Uniwrap from './uniwrap';
 
 let instance = null;
 
-const root = `${process.cwd()}/.uniwraprc`
+const root = `${process.cwd()}.uniwraprc`
 console.log('Root:', root);
 const wrp = new Uniwrap(root);
 
