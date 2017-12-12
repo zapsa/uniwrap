fcbf9d8caa85ae0873837ac5c55a60c6bd08cd29
Quentin Hausser <hausser.quentin@gmail.com>
2017-12-12 16:17:25 +0100

    Hot fix

--- dist/wrapper.js
+++ dist/wrapper.js
@@ -188,7 +188,7 @@ var Wrapper = function () {
                     throw new Error('No response');
                   }
                   if (!response.ok) {
-                    throw { message: 'Request error: status is ' + response.status + ' (' + response.statusText + ')', status: response.status };
+                    throw (0, _extends3.default)({}, response, { message: 'Request error: status is ' + response.status + ' (' + response.statusText + ')', status: response.status });
                   }
                   if (response.status === 204 || _this.def.routes[name].responseType && _this.def.routes[name].responseType === 'no-content') {
                     return null;
--- src/wrapper.js
+++ src/wrapper.js
@@ -72,7 +72,7 @@ class Wrapper {
         throw new Error('No response');
       }
       if (!response.ok) {
-        throw { message: `Request error: status is ${response.status} (${response.statusText})`, status: response.status };
+        throw { ...response, message: `Request error: status is ${response.status} (${response.statusText})`, status: response.status,  };
       }
       if (response.status === 204 || this.def.routes[name].responseType && this.def.routes[name].responseType === 'no-content') {
           return null;
